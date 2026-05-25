84f4d88738bb0e9a916c1a27682bdc6dc3650d74
ioRM-b <ramosiovanni@gmail.com>
2026-05-24 19:34:14 -0600

    documentación swagger de los métodos put

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const port = 3000;
 const { sequelize } = require('./src/connection');
 const { Alumno, Entidad, Profesor, Asignatura, Plantel } = require('./src/models'); 
 
-const swaggerUi = require('swagger-ui-express');
-const specs = require('./swagger/swagger.js');
-
-//import swaggerUi from 'swagger-ui-express'; //importamos swagger UI express, podría ser que haya que agregar gzipped
-//import specs from "./swagger/swagger.js"; //importamos los specs desde nuestro swagger.js
+const swaggerUi = require('swagger-ui-express'); //importamos swagger UI express
+const specs = require('./swagger/swagger.js'); //import specs from "./swagger/swagger.js"; //importamos los specs desde nuestro swagger.js
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs)) //handler para la documentación en el formato que pide swagger 
 // la ruta de la documentación va a ser api-docs 
